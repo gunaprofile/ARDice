5413b714a3a794c7980d42412382e9afd8a756d1
gunm <gunm@cisco.com>
2018-04-04 06:42:42 +0530

    Animate and Roll all dice at once

--- ARDice/ViewController.swift
+++ ARDice/ViewController.swift
@@ -11,6 +11,8 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    var diceArray = [SCNNode]()
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -83,22 +85,38 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
         
                     diceNode.position = SCNVector3(x : hitResult.worldTransform.columns.3.x, y : hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius, z : hitResult.worldTransform.columns.3.z)
+                    
+                    diceArray.append(diceNode)
         
                     sceneView.scene.rootNode.addChildNode(diceNode)
                     
-                    let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
-                    
-                    let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
-                    
-                    diceNode.runAction(SCNAction.rotateBy(
-                        x: CGFloat(randomX * 5),
-                        y: 0,
-                        z: CGFloat(randomZ * 5),
-                        duration: 0.5
-                    ))
+                    roll(dice: diceNode)
                 }
             }
         }
+    }
+    
+    func rollAll(){
+        if !diceArray.isEmpty{
+            for dice in diceArray {
+                roll(dice : dice)
+            }
+        }
+    }
+    
+    func roll(dice: SCNNode){
+        
+        let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        
+        let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        
+        dice.runAction(SCNAction.rotateBy(
+            x: CGFloat(randomX * 5),
+            y: 0,
+            z: CGFloat(randomZ * 5),
+            duration: 0.5
+        ))
+        
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
@@ -126,6 +144,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             return
         }
         
+    }
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        rollAll()
     }
     
 }
